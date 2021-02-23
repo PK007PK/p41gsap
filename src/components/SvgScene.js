9d71d0c58d4aed85d8a8b0e0cbc98407940f0395
Piotr Krasny <krasny.piotr@gmail.com>
2021-02-23 12:30:39 +0100

    Add SvgScene

--- src/components/SvgScene.js
+++ src/components/SvgScene.js
@@ -1,8 +1,26 @@
 import { useEffect, useRef } from "react";
 import { ReactComponent as Scene } from "../assets/images/scene.svg";
-
+import { gsap } from "gsap";
 export default function SvgScene() {
   const wrapper = useRef(null);
+
+  useEffect(() => {
+    const [elements] = wrapper.current.children;
+    const person = elements.getElementById("Person");
+    const task1 = elements.getElementById("Task1");
+    const task2 = elements.getElementById("Task2");
+    const task3 = elements.getElementById("Task3");
+
+    gsap.set([person, task1, task2, task3], { autoAlpha: 0 });
+    gsap.set([task1, task2, task3], { transformOrigin: "50% 50%" });
+
+    const tl = gsap.timeline({ defaults: { ease: "power3.inOut" } });
+    tl.fromTo(person, { x: "-=300" }, { duration: 1, x: "+=300", autoAlpha: 1 })
+      .fromTo(task1, { scale: 0 }, { duration: 0.5, scale: 1, autoAlpha: 1 })
+      .fromTo(task2, { scale: 0 }, { duration: 0.5, scale: 1, autoAlpha: 1 })
+      .fromTo(task3, { scale: 0 }, { duration: 0.5, scale: 1, autoAlpha: 1 });
+  });
+
   return (
     <div ref={wrapper} className="flex">
       <Scene className="mx-auto" />
