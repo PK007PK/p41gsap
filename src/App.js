c7440cedb3293975b96441018e3c2a9af81d1119
Piotr Krasny <krasny.piotr@gmail.com>
2021-02-24 12:08:17 +0100

    Save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { useContext } from "react";
-import styled from "styled-components";
-import AppProvider, { AppContext } from "./AppContext";
+import AppProvider from "./AppContext";
 import Layout from "./components/Layout";
 import Menu from "./components/Menu";
 
@@ -8,20 +6,6 @@ import GlobalStyles from "./components/styles/GlobalStyles";
 import SvgScene from "./components/SvgScene";
 import SvgScene2 from "./components/SvgScene2";
 import SvgScene3 from "./components/SvgScene3";
-
-const StyledDiv = styled.div`
-  color: var(--black);
-  ${({ theme }) => theme.media.lgAbove} {
-    color: red;
-  }
-`;
-
-const Button = () => {
-  const { toogleIsActive, isActive } = useContext(AppContext);
-  return (
-    <button onClick={toogleIsActive}>{isActive ? "True" : "False"}</button>
-  );
-};
 
 function App() {
   return (
